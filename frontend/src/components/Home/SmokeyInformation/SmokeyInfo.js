d673fbd82de1d72878ac13460c7468063b5f1b95
Christopher Youn <christopheryoun@Christophers-MacBook-Air.local>
2021-02-20 11:23:27 -0800

    Add Animations

--- frontend/src/components/Home/SmokeyInformation/SmokeyInfo.js
+++ frontend/src/components/Home/SmokeyInformation/SmokeyInfo.js
@@ -1,25 +1,54 @@
 import './SmokeyInfo.scss'
+import {useRef} from 'react'
+import {useIntersection} from 'react-use'
+import gsap from 'gsap'
 
 
 //TODO: Make responsive at 960px breakpoint
 const SmokeyInfo = () => {
+
+	//TODO: Refactor this to be reusable
+	const sectionRef = useRef(null)
+
+	const intersection = useIntersection(sectionRef, {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.9
+	})
+
+
+	const intersecting = intersection && intersection.intersectionRatio >= 0.9
+
+	if (intersecting) fadeIn()
+
 	return (
-		<section className='smokeyinfo flex'>
+		<section className='smokeyinfo flex' ref={sectionRef}>
 			<div className='container flex ai-center'>
 				<div className='smokeyinfo__txt'>
-					<h2 classname='smokeyinfo__header'>Say Hello to Smokey</h2>
-					<p className='smokeyinfo__body'>Welcome to the National Park Service chat bot Smokey!
+					<h2 className='smokeyinfo__header fadeIn2'>Say Hello to Smokey</h2>
+					<p className='smokeyinfo__body fadeIn2'>Welcome to the National Park Service chat bot Smokey!
 					Simply ask the chat bot a question about a service offered
 					by a national park and receive information quick!
 					Ask anything ranging from campground information, entry fees, and
 					park hours, and Smokey will answer!</p>
 				</div>
 				<div className='smokeyinfo__img flex jc-fend'>
-					<img className='exampleimage' src="/ChatExample.png" />
+					<img className='exampleimage fadeIn2' src="/ChatExample.png" />
 				</div>
 			</div>
 		</section>
 	)
+}
+
+
+const fadeIn = () => {
+	gsap.to('.fadeIn2', 0.5, {
+		opacity: 1,
+		y: 0,
+		stagger: {
+			amount: 0.3
+		}
+	})
 }
 
 export default SmokeyInfo
--- frontend/src/components/Home/SmokeyLink/SmokeyLink.js
+++ frontend/src/components/Home/SmokeyLink/SmokeyLink.js
@@ -2,6 +2,9 @@ import './SmokeyLink.scss'
 import Button from '@material-ui/core/Button'
 import {makeStyles} from '@material-ui/core/styles'
 import {useHistory} from 'react-router-dom'
+import {useIntersection} from 'react-use'
+import {useRef} from 'react'
+import gsap from 'gsap'
 
 
 
@@ -22,21 +25,48 @@ const useStyle = makeStyles({
 
 const SmokeyLink = () => {
 	const classes = useStyle()
-	const history=useHistory()
+	const history = useHistory()
 
+
+	const sectionRef = useRef(null)
+
+	const intersection = useIntersection(sectionRef, {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.5
+	})
+
+
+	const intersecting = intersection && intersection.intersectionRatio >= 0.6
+	console.log(intersecting)
+
+	//TODO: Remove the console.log
+	if (intersecting) fadeIn()
+
+	//TODO: Animations not working for some reason
 	return (
-		<section className='smokeylink flex jc-center ai-center'>
+		<section className='smokeylink flex jc-center ai-center' >
 			<div className='container flex flex-column ai-center'>
-				<h1>Talk to our Virtual Assistant, Smokey</h1>
+				<h1 className='fadeIn3'>Talk to our Virtual Assistant, Smokey</h1>
 				<Button
 					variant='outlined'
-					className={classes.outlined}
-					onClick={()=>history.push('/chat')}
+					className={`${classes.outlined} fadeIn3`}
+					onClick={() => history.push('/chat')}
 				>Chat Now</Button>
 			</div>
-			<div className="footer"></div>
 		</section>
 	)
+}
+
+
+const fadeIn = () => {
+	gsap.to('.fadeIn3', 0.6, {
+		opacity: 1,
+		y: 0,
+		stagger: {
+			amount: 0.3
+		}
+	})
 }
 
 export default SmokeyLink
