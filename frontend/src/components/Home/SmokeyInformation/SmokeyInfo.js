3914cc4b859b1907d8d992f946912a75094ea113
Christopher Youn <cyoun@chapman.edu>
2021-02-19 19:04:02 -0800

    Refactor SmokeyInfo Component

--- frontend/src/components/Home/SmokeyInformation/SmokeyInfo.js
+++ frontend/src/components/Home/SmokeyInformation/SmokeyInfo.js
@@ -1,20 +1,23 @@
 import './SmokeyInfo.scss'
 
 
+//TODO: Make responsive at 960px breakpoint
 const SmokeyInfo = () => {
 	return (
-		<section className='smokeyinfo'>
-			<div className='container'>
-				<h1 classname = 'header'>Say Hello to Smokey</h1>
-                <p className = 'textinfo'>Welcome to the National Park Service chat bot Smokey! 
-                    Simply ask the chat bot a question about a service offered 
-                    by a national park and receive information quick! 
-                    Ask anything ranging from campground information, entry fees, and 
-                    park hours, and Smokey will answer!</p>
+		<section className='smokeyinfo flex'>
+			<div className='container flex ai-center'>
+				<div className='smokeyinfo__txt'>
+					<h2 classname='smokeyinfo__header'>Say Hello to Smokey</h2>
+					<p className='smokeyinfo__body'>Welcome to the National Park Service chat bot Smokey!
+					Simply ask the chat bot a question about a service offered
+					by a national park and receive information quick!
+					Ask anything ranging from campground information, entry fees, and
+					park hours, and Smokey will answer!</p>
+				</div>
+				<div className='smokeyinfo__img flex jc-fend'>
+					<img className='exampleimage' src="/ChatExample.png" />
+				</div>
 			</div>
-            <div>
-                <img className = 'exampleimage' src="/ChatExample.png" />
-            </div>
 		</section>
 	)
 }
