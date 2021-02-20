bbf8c4c81504b21c777a5b32d0bfbed589cff408
wonderhead <mitch.radman101@gmail.com>
2021-02-20 15:26:23 -0800

    created parkcode dict that stores park names/codes

--- backend/models/request.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-//make a schema for the http req from the chatbot
-const park = new Schema({
-    parkCodes : {parkName : String, parkCode : String}
-});
