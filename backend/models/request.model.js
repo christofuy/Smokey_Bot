689923b9d1ac24e77036366b2964d18d16689949
wonderhead <mitch.radman101@gmail.com>
2021-02-19 19:57:58 -0800

    created park schema

--- backend/models/request.model.js
+++ backend/models/request.model.js
@@ -2,3 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //make a schema for the http req from the chatbot
+const park = new Schema({
+    parkCodes : {parkName : String, parkCode : String}
+});
