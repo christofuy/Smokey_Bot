dd7665ab5062bc36d9cde027a4abc23875db3b10
Nicholas Kim <nick_kim00@hotmail.com>
2021-02-19 16:09:34 -0800

    Added SmokeyLink

---
+++ frontend/src/components/Home/SmokeyLink/SmokeyLink.js
@@ -0,0 +1,15 @@
+import './SmokeyLink.scss'
+
+
+const SmokeyLink = () => {
+	return (
+		<section className='smokeylink'>
+			<div className='container'>
+				<h1>Talk to our Virtual Assistant, Smokey</h1>
+                <button className='chatbutton'>Chat Now</button>
+			</div>
+		</section>
+	)
+}
+
+export default SmokeyLink
