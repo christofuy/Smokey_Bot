d140bd7dfaeff092a3ea4b479ca56773fbfa0824
Christopher Youn <christopheryoun@Christophers-MacBook-Air.local>
2021-02-20 22:48:27 -0800

    Refactor Footer

--- frontend/src/components/Home/Footer/Footer.js
+++ frontend/src/components/Home/Footer/Footer.js
@@ -1,22 +1,52 @@
 import './footer.scss'
 
+
 const Footer = () => {
 	return (
-		<footer className="footer">
-			<h1 className="footer-text">To continue showing your support to our parks, please feel free to visit the following resources</h1>
-			<span className="footer-txt">
-				<p className="donatetxt">Donate</p>
-				<p className="supporttxt"><t>Support</t></p>
-				<p className="volunteertxt"><t>Volunteer</t></p>
-			</span>
-			<ul className="footer-links">
-				<li className="donate"><a href="https://www.nps.gov/subjects/partnerships/donate.htm" target="_blank"><img className="logo" src="/npsLogo.png"></img></a></li>
-				<li className="support"><a href="https://www.nationalparks.org/" target="_blank"><img className="logo" src="/npfLogo.png"></img></a></li>
-				<li className="volunteer"><a href="https://www.nps.gov/getinvolved/volunteer.htm" target="_blank"><img className="logo" src="/VIPLogo1.png"></img></a></li>
-			</ul>
+		<footer className='footer'>
+			<div className='container'>
+				<h4 className="footer-text">To continue showing your support to our parks, please feel free to visit the following resources</h4>
+				<div className='footer__row flex'>
+					{FOOTERLINKS.map(props => (
+						<FooterCol {...props} key={props.text} />
+					))}
+				</div>
+			</div>
 		</footer>
 	)
 }
+
+
+const FooterCol = ({link, text, imgPath}) => {
+	return (
+		<div className='footer__col flex flex-column ai-center'>
+			<p>{text}</p>
+			<a href={link} target='_blank'>
+				<img className="logo" src={imgPath} />
+			</a>
+		</div>
+
+	)
+}
+
+
+const FOOTERLINKS = [
+	{
+		link: 'https://www.nps.gov/subjects/partnerships/donate.htm',
+		text: 'Donate',
+		imgPath: '/npsLogo.png'
+	},
+	{
+		link: 'https://www.nationalparks.org/',
+		text: 'Support',
+		imgPath: 'npfLogo.png'
+	},
+	{
+		link: 'https://www.nps.gov/getinvolved/volunteer.htm',
+		text: 'Volunteer',
+		imgPath: '/VIPLogo1.png'
+	}
+]
 
 
 export default Footer
