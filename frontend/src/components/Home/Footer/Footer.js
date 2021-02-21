5e13f2337cc08bd836bf4c9e5c8e07f577988599
Christopher Youn <christopheryoun@Christophers-MacBook-Air.local>
2021-02-20 16:01:37 -0800

    Modularize Footer Component

---
+++ frontend/src/components/Home/Footer/Footer.js
@@ -0,0 +1,18 @@
+
+
+
+const Footer = () => {
+	return (
+		<footer id="footer">
+			<h1 id="footer-text">To continue showing your support to our parks, please feel free to visit the following resources</h1>
+			<ul id="footer-links">
+				<li id="nps-test"><a href="https://www.nps.gov/subjects/partnerships/donate.htm" target="_blank">National Park Service Donation<img className="logo" src="/npsLogo.png"></img></a></li>
+				<li><a href="https://www.nationalparks.org/" target="_blank">Support the National Park Foundation<img className="logo" src="/npfLogo.png"></img></a></li>
+				<li><a href="https://www.nps.gov/getinvolved/volunteer.htm" target="_blank">NPS Volunteer-In-Parks Program<img className="logo" src="/VIPLogo1.png"></img></a></li>
+			</ul>
+		</footer>
+	)
+}
+
+
+export default Footer
