782139f2b00651daf99e0003557b0b1fae5e2ab5
wonderhead <mitch.radman101@gmail.com>
2021-02-21 01:45:33 -0800

    added grand canyon

--- backend/dicts/parkcode_dict.js
+++ backend/dicts/parkcode_dict.js
@@ -1,7 +1,7 @@
 var parkCode_dict = {'Acadia National Park' : 'acad','Arches National Park' : 'arch','Badlands National Park' : 'badl','Big Bend National Park' : 'bibe','Biscayne National Park' : 'bisc', 'Black Canyon of the Gunnison National Park' :
 'blca','Bryce Canyon National Park' : 'brca','Canyonlands National Park' : 'cany','Capitol Reef National Park' : 'care','Carlsbad Caverns National Park' : 'cave', 'Channel Islands National Park' : 'chis',
 'Crater Lake National Park' : 'crla','Cuyahoga Valley National Park' : 'cuva','Denali National Park' : 'dena','Death Valley National Park' : 'deva','Dry Tortugas National Park' : 'drto','Everglades National Park' : 'ever',
-'Gates of the Arctic National Park' : 'gaar','Glacier National Park' : 'glac','Great Basic National Park' : 'grba','Great Smokey Mountains National Park':'grsm','Grand Teton National Park' : 'grte',
+'Gates of the Arctic National Park' : 'gaar','Glacier National Park' : 'glac','Great Basic National Park' : 'grba','Great Smokey Mountains National Park':'grsm','Grand Canyon National Park' : 'grca','Grand Teton National Park' : 'grte',
 'Guadalupe Mountains National Park' : 'gumo','Haleakala National Park' : 'hale', 'Hawaii Volcanoes National Park' : 'havo','Hot Springs National Park' : 'hosp','Isle Royale National Park' : 'isro',
 'Joshua Tree National Park' : 'jotr','Katmai National Park' : 'katm','Kenai Fjords National Park' : 'kefj','Kings Canyon National Park' : 'kica','Kobuk Valley National Park' : 'kova','Lake Clark National Park' : 'lacl',
 'Lassen Volcanic National Park' : 'lavo','Mammoth Cave National Park' : 'maca','Mesa Verde National Park' : 'meve','Mount Rainier National Park' : 'mora','North Cascades National Park' : 'noca','Olympic National Park' : 'olym',
