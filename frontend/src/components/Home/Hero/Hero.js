a687d571e5421b105d1cd0de49f1be73a3b95712
Christopher Youn <christopheryoun@Christophers-MacBook-Air.local>
2021-02-20 21:24:52 -0800

    Add Parallax Effect

--- frontend/src/components/Home/Hero/Hero.js
+++ frontend/src/components/Home/Hero/Hero.js
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react'
 import './hero.scss'
 import ButtonOutlined from '../../Utils/Buttons'
 import {Link as ScrollLink} from 'react-scroll'
@@ -7,15 +8,27 @@ import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
 
 
 const Hero = () => {
+	const [offsetY, setOffsetY] = useState(0)
+	const handleScroll = () => setOffsetY(window.pageYOffset)
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [])
 
 	return (
 		<section className='hero'
 			//How do I access img in public in scss
-			//TODO: Resize img to be more performant
 			style={{background: 'linear-gradient(180deg, rgba(255, 255, 255, 0) 30.86%, #FFF 96.68%),url("TallTrees.jpg") no-repeat scroll center top / cover'}}
 		>
-			<div className='container flex flex-column ai-center'>
-				<h1>Visit Your Favorite National Park with Ease</h1>
+			<div
+				className='container flex flex-column ai-center'
+				style={{
+					transform: `translateY(${-offsetY * 0.5}px)`,
+					willChange: 'transform'
+				}}
+			>
+				<h1 >Visit Your Favorite National Park with Ease</h1>
 				<ScrollLink to='mission' spy smooth duration={1000}>
 					<ButtonOutlined>Learn More</ButtonOutlined>
 				</ScrollLink>
@@ -42,6 +55,8 @@ const TOC = () => {
 		</div>
 	)
 }
+
+
 
 
 export default Hero
